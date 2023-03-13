50e8bc49eb60939a19b48d83f767c84ca83fedf1
ftatulich <kartieltv@gmail.com>
2023-03-13 10:54:51 +0200

    fix api url

--- SPACommentsBackend/SPAComments/urls.py
+++ SPACommentsBackend/SPAComments/urls.py
@@ -13,5 +13,4 @@ urlpatterns = [
     path('api/v1/', include('comments.urls'))
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
